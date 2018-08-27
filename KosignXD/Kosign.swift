0908dd05e6cba41c67a2a218e2caddd7c8b67cdd
Vansa Pha <vs.lov.rs@gmail.com>
2018-08-27 11:14:38 +0700

    init

---
+++ KosignXD/Kosign.swift
@@ -0,0 +1,23 @@
+//
+//  Kosign.swift
+//  KosignXD
+//
+//  Created by Vansa Pha on 8/27/18.
+//  Copyright © 2018 KOSIGN. All rights reserved.
+//
+
+import Foundation
+
+public class Kosign {
+    
+    private init() {}
+    
+    public class func add(v1: Int, v2: Int) -> Int {
+        return v1 + v2
+    }
+    
+    public class func sub(v1: Int, v2: Int) -> Int {
+        return v1 - v2
+    }
+    
+}
---
+++ KosignXD/KosignXD.h
@@ -0,0 +1,19 @@
+//
+//  KosignXD.h
+//  KosignXD
+//
+//  Created by Vansa Pha on 8/27/18.
+//  Copyright © 2018 KOSIGN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KosignXD.
+FOUNDATION_EXPORT double KosignXDVersionNumber;
+
+//! Project version string for KosignXD.
+FOUNDATION_EXPORT const unsigned char KosignXDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KosignXD/PublicHeader.h>
+
+
